27f7736f6a5d3c081fb5562e6b1fafb2859dc85a
Ficelo <teiva.trillard@gmail.com>
2025-06-08 16:15:15 +0200

    Added warning message (final commit surely)

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './component/header/header.component';
 import {FooterComponent} from './component/footer/footer.component';
+import {Dialog} from 'primeng/dialog';
+import {Button} from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, Dialog, Button],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'mainTendue';
+
+  display: boolean = false;
+
+  ngOnInit() {
+
+    const shown = localStorage.getItem('showDialog');
+    console.log(shown);
+
+    if(!shown) {
+      this.display = true;
+    }
+
+  }
+
+  showDialog() {
+    localStorage.setItem('showDialog', "true")
+    this.display = false;
+  }
+
 }
